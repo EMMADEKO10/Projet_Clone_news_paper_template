1dfddfc72a518dc36296607f73e6f08d978e4969
chris <christianmabealdi@gmail.com>
2023-12-27 10:34:46 +0100

    fix directory for blogsdata.json

--- server/index.js
+++ server/index.js
@@ -5,11 +5,26 @@ import cors from "cors";
 // middleware
 app.use(cors());
 app.use(json());
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Bienvenue dans mon blog!')
 })
+
+
+app.get("/articles", async (req, res) => {
+    try {
+      // Utilisez "import()" avec l'option "assertion" pour importer le JSON
+      const data = await import("../api/blogsData.json", { assert: { type: 'json' } });
+      res.send(data);
+    } catch (error) {
+      console.error("Error reading JSON file:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+
 
 app.listen(port, () => {
   console.log(`L'application tourne sur ${port}`)
