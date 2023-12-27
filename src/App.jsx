1d02426061114d97cae5b5e6c455894ce92a4227
EMMADEKO10 <emmanueldeko64@gmail.com>
2023-12-27 09:05:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './style/App.css'
+import Header from './components/header'
+import Rectangle1 from './components/Rectangle1'
+import Home from './components/home'
+import Header2 from './components/header2'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Rectangle1></Rectangle1>
+      <Header2 />
+      <Home />;
+    </>
+  )
+}
+
+export default App
