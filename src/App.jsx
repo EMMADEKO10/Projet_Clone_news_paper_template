63515efafba0d2fe45e9f7b2fd8669dfe60650c2
EMMADEKO10 <emmanueldeko64@gmail.com>
2023-12-29 15:41:28 +0100

    Mes modifications locales

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import './style/App.css'
-import Header from './components/header'
-import Rectangle1 from './components/Rectangle1'
-import Home from './components/home'
-import Header2 from './components/header2'
+import Home from "./pages/home";
+import Blog from "./pages/blog";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 
-function App() {
+// const data = require('../api/blogsData.json');
+
+export default function App() {
 
   return (
-    <>
-      <Header></Header>
-      <Rectangle1></Rectangle1>
-      <Header2 />
-      <Home />;
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <BlogsProvider>
+        <Routes>
+  
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+
+        </Routes>
+        </BlogsProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export const BlogsContext = React.createContext({
+  useDataBlog: [],
+});
+
+export function BlogsProvider({ children }) {
+  const [useDataBlog, setUseDataBlog] = useState();
+
+  return (
+    <BlogsContext.Provider value={{ useDataBlog, setUseDataBlog}}>
+      {children}
+    </BlogsContext.Provider>
+  );
+}
